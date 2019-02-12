e6931d70a66484a9878d54b05b3d45987cb04e6b
Scott Sinclair <scottysinclair@gmail.com>
2019-02-12 17:25:02 +0100

    added custom query for a months transactions.

--- src/clojback/barleydb.clj
+++ src/clojback/barleydb.clj
@@ -1,6 +1,8 @@
 (ns clojback.barleydb)
 
 (import 'scott.barleydb.server.jdbc.persist.QuickHackSequenceGenerator)
+(import 'scott.barleydb.api.query.QueryObject)
+(import 'scott.barleydb.api.core.types.JavaType)
 
 (def env-definition
     (-> (scott.barleydb.bootstrap.EnvironmentDef/build)
@@ -24,7 +26,42 @@
 
 ;	(first (remove #(nil? (.getDefinitionsSpec % namespace)) (-> env-definition (.getAllSpecRegistries)))))
 
+
+(defn query-property [query name] 
+	(new scott.barleydb.api.query.QProperty query name))
+
+(defn query-where [query cond] 
+	(.where query cond))
+
+(defn parse-date [date-str date-format]
+	(-> (new java.text.SimpleDateFormat date-format)
+		(.parse date-str)))
+
+(defn todate[date-str] 
+	(parse-date date-str "dd-MM-yyyy"))
+
+(defn param [name type]
+	(new scott.barleydb.api.query.QParameter name type))
+
+(defn query-transactions-in-month []
+	(let [query (new QueryObject "scott.data.model.Transaction")
+		  where (partial query-where query)
+		  prop (partial query-property query)
+		 ]
+		(-> (where (-> (prop "date")
+			     (.greaterOrEqualParam (param "fromDate" JavaType/UTIL_DATE))
+			    ))
+		    (.and (-> (prop "date")
+		    	 (.lessParam (param "toDate" JavaType/UTIL_DATE))
+		    	 ))
+		 )))
+
+
+(defn custom-queries []
+	(doto (new scott.barleydb.build.specification.graphql.CustomQueries)
+		(.register "transactionsInMonth" (query-transactions-in-month))))
+
 (defn get-graphql-schema [namespace] 
-	(new scott.barleydb.api.graphql.BarleyGraphQLSchema (get-spec-registry) env namespace nil))
+	(new scott.barleydb.api.graphql.BarleyGraphQLSchema (get-spec-registry) env namespace (custom-queries)))
 
 
